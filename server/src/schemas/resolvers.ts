02220ce262c5a87768975f9085007c1ae4faa443
Ben Johnson <johben17796@gmail.com>
2025-02-20 20:28:05 -0600

    honestly not much changed here, query is in the way we want it formatted, applying the argument to give a specific set name comes next, right now that doesn't work lol.

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,3 +1,56 @@
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+// // import User from '../models/User';
+
+// const resolvers = {
+//     Query: {
+//         openSinglePack: async (_parent: any, _args: any, _context: any) => {
+//             // redefine cards array
+//             // instead of loading from cards.json
+//             // make a fetch call instead to pokemon ap
+        
+//             const res = await fetch('https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:[1 TO 151]&pageSize=151', {
+//                 headers: {
+//                     'X-Api-Key': process.env.POKEMON_API_KEY || '',
+//                 },
+//             });
+//             const cards = await res.json();
+//             console.log(cards);
+//             const randomCards = [];
+
+//             for (let i = 0; i < 10; i++) {
+//                 const randomCard = cards.data[Math.floor(Math.random() * cards.data.length)];
+//                 randomCards.push(randomCard);
+//             }
+            
+//             return randomCards;
+//         },
+//     },
+
+//     Mutation: {
+//         saveCardToBinder: async (_parent: any, _args: any, _context: any) => {
+//             // const updateUser = await User.findOneAndUpdate(
+//             //     { _id: _args.userId },
+//             //     { $push: { cards: _args.cardId } },
+//             //     { new: true }
+//             // );
+            
+            
+//             return "Hi"
+//         },
+//         removeCardFromBinder: async (_parent: any, _args: any, _context: any) => {
+//             return "Hey"
+//         },
+//     },
+// };
+
+
+
+
+// export default resolvers;
+
+// code for selecting from a certain set.
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -5,12 +58,14 @@ dotenv.config();
 
 const resolvers = {
     Query: {
-        openSinglePack: async (_parent: any, _args: any, _context: any) => {
+        openSinglePack: async (_parent: any, args: any, _context: any) => {
             // redefine cards array
             // instead of loading from cards.json
-            // make a fetch call instead to pokemon ap
-        
-            const res = await fetch('https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:[1 TO 151]&pageSize=151', {
+            // make a fetch call instead to pokemon api
+            // should be args.selection, this IS NOT CORRECTLY IMPLEMENTED AT THE MOMENT D: 
+            const selectedSet=args.selection || "base1"
+            console.log(selectedSet)
+            const res = await fetch(`https://api.pokemontcg.io/v2/cards?q=set.id:${selectedSet} supertype:pokemon`, {
                 headers: {
                     'X-Api-Key': process.env.POKEMON_API_KEY || '',
                 },
@@ -37,7 +92,7 @@ const resolvers = {
             // );
             
             
-            return "Hi"
+            return "Hi, hey"
         },
         removeCardFromBinder: async (_parent: any, _args: any, _context: any) => {
             return "Hey"
