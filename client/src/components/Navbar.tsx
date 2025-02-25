12275a8c4188325237961e6f1cd85f898f65bd12
kavue <kvue06@gmail.com>
2025-02-24 19:09:23 -0600

    Added logout function and moved files

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
 import { useState } from 'react';
-import LoginModal from './LoginModal';
+import LoginModal from './LoginModal'; 
+import Auth from '../utils/auth'; // Import the Auth utility
 
 const Navbar = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -8,14 +9,26 @@ const Navbar = () => {
   const openModal = () => setModalVisible(true);
   const closeModal = () => setModalVisible(false);
 
+  const handleLogout = () => {
+    Auth.logout(); // Logs out user and removes token
+    window.location.assign('/'); // Redirects to home after logout
+  };
+
   return (
     <nav>
       <h1>
-        <Link to='/' id='logo'>
-          <img src='.\pokpak_logo.svg' alt='PokePack!' />
+        <Link to="/" id="logo">
+          PokePack!
         </Link>
       </h1>
       <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <h2>
+            <Link to="/" className="nav-link">
+              HOME
+            </Link>
+          </h2>
+        </li>
         <li className="nav-item">
           <h2>
             <Link to="/Collection" className="nav-link">
@@ -23,11 +36,26 @@ const Navbar = () => {
             </Link>
           </h2>
         </li>
-        <li className="nav-item">
-            <button onClick={openModal} className="nav-link">
-              Login / Sign Up
-            </button>
-        </li>
+
+        {Auth.loggedIn() ? (
+          // If logged in, show Logout button
+          <li className="nav-item">
+            <h2>
+              <button onClick={handleLogout} className="nav-link">
+                Logout
+              </button>
+            </h2>
+          </li>
+        ) : (
+          // If not logged in, show Login / Sign Up button
+          <li className="nav-item">
+            <h2>
+              <button onClick={openModal} className="nav-link">
+                Login / Sign Up
+              </button>
+            </h2>
+          </li>
+        )}
       </ul>
 
       {/* Modal */}
