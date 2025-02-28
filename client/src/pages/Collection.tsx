a8d9576bde2bff3339f863c209444510e9c5bff5
allenarnoldy <allenarnoldy@gmail.com>
2025-02-28 17:55:42 -0600

    got the cards to display in the binder

--- client/src/pages/Collection.tsx
+++ client/src/pages/Collection.tsx
@@ -1,55 +1,16 @@
 import PokemonCard from "../components/PokemonCard";
-const cardArray = [
-    {
-        name: "Gastly",
-        level: "undefined",
-        types: [ "Psychic" ],
-        setName: "151",
-        rarity: "Common",
-        imageUrl: "https://images.pokemontcg.io/sv3pt5/92_hires.png"
-      },
-      {
-        name: "Gengar",
-        level: "undefined",
-        types: [ "Psychic" ],
-        setName: "151",
-        rarity: "Rare",
-        imageUrl: "https://images.pokemontcg.io/sv3pt5/94_hires.png"
-      },
-      {
-        name: "Onix",
-        level: "undefined",
-        types: [ "Fighting" ],
-        setName: "151",
-        rarity: "Uncommon",
-        imageUrl: "https://images.pokemontcg.io/sv3pt5/95_hires.png",
-        nickname: "Rockey"
-      },
-      {
-        name: "Growlithe",
-        level: "undefined",
-        types: [ "Fire" ],
-        setName: "151",
-        rarity: "Common",
-        imageUrl: "https://images.pokemontcg.io/sv3pt5/58_hires.png"
-      },
-      {
-        name: "Kadabra",
-        level: "undefined",
-        types: [ "Psychic" ],
-        setName: "151",
-        rarity: "Uncommon",
-        imageUrl: "https://images.pokemontcg.io/sv3pt5/64_hires.png"
-      }
-];
+import { useQuery } from "@apollo/client";
+import { LOGIN_USER } from "../utils/queries";
 
 const Collection = () => {
+  const { data } = useQuery(LOGIN_USER);
+  console.log(data);
     return (
         <>
             <h1 className="binder-header">Your Binder</h1>
             <div className="binder">
                 {
-                    cardArray.map(function (card: any) {
+                    data?.me.binder.map(function (card: any) {
                         return (
                             <PokemonCard pokemon={card}/>
                         )
