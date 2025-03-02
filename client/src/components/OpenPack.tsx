397b53e79875ba6225470963f35b9fdf49f94d39
Matt Oberdalhoff <reliable.vagabond@gmail.com>
2025-03-02 13:28:34 -0600

    added css and js code to have pack appear in an appealing way

--- client/src/components/OpenPack.tsx
+++ client/src/components/OpenPack.tsx
@@ -1,6 +1,6 @@
 import { useQuery, useLazyQuery, useMutation } from "@apollo/client";
 import { OPEN_SINGLE_PACK } from "../utils/queries";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SAVE_CARD } from "../utils/mutations.js";
 
 
@@ -16,29 +16,39 @@ const OpenPack = () => {
         setSelectedPack(e.target.value);
     }
 
+    const animatePack = () => {
+        const cards = document.querySelectorAll('.packCard');
+        cards.forEach(card => {
+            card.classList.add('open');
+        })
+    }
+
     async function handleSubmit(event: any) {
         event.preventDefault();
         const form = event.target;
         const packSelect = new FormData(form);
         const packJson = Object.fromEntries(packSelect.entries())
         const { data: packData } = await getPack({variables: {setName: packJson.selectedPack}});
-        const randomCards = packData?.openSinglePack || [];
+        const randomCards = packData?.openSinglePack || []; // TODO change back to '[]' once done testing
         // TODO use 'selectedPack' from submitted form to run card finding and pack opening
         for (let i = 0; i < 5; i++) {
          saveCard({variables: {cardId: randomCards[i]._id, userId: localStorage.getItem('userId')}});
         }
         setRandomCards(randomCards);
+        setTimeout(animatePack,1000);
     };
     // TODO current list values to be replaced with for loop that uses a passed in array to populate the options
     return (
         <>
             <div className="pack-container">
+                <div className="booster">
                 { randomCards.map((card: any) => (
-                    <div key={card._id} className="card">
+                    <div key={card._id} className='packCard'>
                         <img src={card.imageUrl} alt={card.name} />
                     </div>
                 ))}
-                <form method="post" onSubmit={handleSubmit}>
+                </div>
+                <form id="packForm" method="post" onSubmit={handleSubmit}>
                     <h1 className="pack-header">TIME TO CRACK A PACK</h1>
                     <label className="pack-list">
                         Choose your Pack!<br></br>
