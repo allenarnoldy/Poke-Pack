772faa8b07c6e8e5edb9eee6178f5730178fc769
allenarnoldy <allenarnoldy@gmail.com>
2025-03-02 10:20:08 -0600

    added a check for no cards in binder

--- client/src/pages/Collection.tsx
+++ client/src/pages/Collection.tsx
@@ -1,4 +1,5 @@
 import PokemonCard from "../components/PokemonCard";
+import { useEffect, useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/queries";
 import { DELETE_CARD } from "../utils/mutations";
@@ -7,12 +8,18 @@ import Auth from "../utils/auth";
 
 const Collection = () => {
   const { loading, data, refetch } = useQuery(LOGIN_USER);
-  console.log("this is the data", data);
-  if (data?.me.binder[0].imageUrl === null) {
-    window.location.reload();
-  };
+  const [hasReloaded, setHasReloaded] = useState(false);
 
   const [removeCardFromBinder] = useMutation(DELETE_CARD);
+
+  useEffect(() => {
+    if (loading && data?.me.binder?.length === 0 && !hasReloaded) {
+      setHasReloaded(true);
+      window.location.reload();
+    }
+  }),
+    [loading, data, hasReloaded];
+
   const handleDeleteCard = async (cardId: string) => {
     if (!Auth.loggedIn()) {
       return;
@@ -31,13 +38,23 @@ const Collection = () => {
     return <h2>Loading...</h2>;
   }
 
+  const hasCards = data?.me.binder?.length > 0;
+
   return (
     <>
       <h1 className="binder-header">Your Binder</h1>
       <div className="binder">
-        {data?.me.binder.map(function (card: any) {
-          return <PokemonCard key={card.id} pokemon={card} onDelete={handleDeleteCard} />;
-        })}
+        {hasCards ? (
+          data.me.binder.map((card: any) => (
+            <PokemonCard
+              key={card.id}
+              pokemon={card}
+              onDelete={handleDeleteCard}
+            />
+          ))
+        ) : (
+          <h2>No cards in binder</h2>
+        )}
       </div>
     </>
   );
